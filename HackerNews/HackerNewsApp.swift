803c30bb7b7b0c1146e9609eb8b2979f645f361d
Varun Bagga <varunbagga19@gmail.com>
2022-08-22 23:58:26 +0530

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Varun Bagga on 22/08/22.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
